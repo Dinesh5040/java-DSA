9d21e0019e5b84ca6b1502fcbfd06edfb8b82d4b
Dinesh5040 <dineshbhukya5040@gmail.com>
2026-08-11 00:09:05 +0530

    updated file

--- Dinesh/01-basics of java/src/com/dinesh/Sum.java
+++ Dinesh/01-basics of java/src/com/dinesh/Sum.java
@@ -1,11 +1,18 @@
 package com.dinesh;
 
+import java.util.Scanner;
+
 public class Sum {
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 45;
-        int sum = a + b;
+//        int a = 10;
+//        int b = 45;
+//        int sum = a + b;
+//        System.out.println("sum  = " + sum);
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
         System.out.println("sum  = " + sum);
 
     }
